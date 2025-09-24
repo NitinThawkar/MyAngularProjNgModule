31b113a483237dec045a06217ee2f08e855b6ed9
Nitinkumar Thawkar <nitin.thawkar@gmail.com>
2025-09-25 00:57:40 +0530

    Add parent and child components for data passing
    Introduced ParentComponent and ChildComponent under passing-data-between-components directory, including their HTML, CSS, and TypeScript files. Registered both components in app.module.ts to prepare for demonstrating data sharing between components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { TypesOfDataBindingComponent } from './types-of-data-binding/types-of-da
 import { StructuralDirectivesComponent } from './structural-directives/structural-directives.component';
 import { CodeTestExampleComponent } from './code-test-example/code-test-example.component';
 import { AttributeDirectivesComponent } from './attribute-directives/attribute-directives.component';
+import { ParentComponent } from './passing-data-between-components/parent.component';
+import { ChildComponent } from './passing-data-between-components/child.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { AttributeDirectivesComponent } from './attribute-directives/attribute-d
     TypesOfDataBindingComponent,
     StructuralDirectivesComponent,
     CodeTestExampleComponent,
-    AttributeDirectivesComponent
+    AttributeDirectivesComponent,
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
