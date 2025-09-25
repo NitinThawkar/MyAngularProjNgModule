0cd318dd3d730326f5804f00844209d3df9959ec
Nitinkumar Thawkar <nitin.thawkar@gmail.com>
2025-09-25 15:37:44 +0530

    template and viewchild
    template and viewchild

--- src/app/template-reference-variable-and-viewchild/template-reference-variable-and-viewchild.component.ts
+++ src/app/template-reference-variable-and-viewchild/template-reference-variable-and-viewchild.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-template-reference-variable-and-viewchild',
   standalone: false,
   templateUrl: './template-reference-variable-and-viewchild.component.html',
-  styleUrl: './template-reference-variable-and-viewchild.component.css'
+  styleUrl: './template-reference-variable-and-viewchild.component.css',
 })
 export class TemplateReferenceVariableAndViewchildComponent {
 
+@ViewChild('myinputElement', {static: true}) myCustomInput:any;
+
+ngOnInit() {
+this.myCustomInput.nativeElement. focus();
+
+}
+
+  sendInput(inputElement: any, paragraph: any) {
+    console.log(inputElement);
+    console.log(inputElement.value);
+    console.log(paragraph);
+    console.log(paragraph.innerText);
+    //paragraph.innerText = inputElement.value;
+  }
 }
